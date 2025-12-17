533278e48b591e28ae965d22960dab49be702700
ams939 <38046122+ams939@users.noreply.github.com>
2025-12-17 13:21:04 +0000

    Deploying to gh-pages from @ ams939/ams939.github.io@1f5e9a8a9ab4e7621b62d96e91da7ecc77c98de5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ams939/ams939",
+    "data-repo": "ams939/ams939.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
